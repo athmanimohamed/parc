fc32c6c66d2c902d45f0db19b46c131de0edf806
moh <gitprogram1975@hotmail.com>
2024-10-09 11:04:27 +0200

    index.html modifier

--- UI/connection.php
+++ UI/connection.php
@@ -1,6 +1,7 @@
 <?php
 try {
     $conne = new PDO('mysql:host=localhost;dbname=', 'root', '');
+    echo ("connecter");
 } catch (Exception $e) {
     die("connection avec sucesse" . $e->getMessage());
 }
